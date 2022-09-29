1837a1381b861c9872dc408969e058367bf7d2e9
stanley <tochukwuu14@gmail.com>
2022-09-29 12:26:10 +0200

    modify index.html file Add some styling to the modification on index file Add javascript codes for the given task

--- src/index.js
+++ src/index.js
@@ -1,53 +1,99 @@
 import './style.css';
+class ToDo {
+  static list = [];
 
-const toDoContainer = document.getElementById('container');
+  constructor(description, complete = false) {
+    this.description = description;
+    this.complete = complete;
+    this.index = ToDo.list.length;
+    ToDo.list.push(this);
+    this.getList = () => ToDo.list;
+  }
 
-const Activities = [
-  {
-    description: 'Say my prayer',
-    completed: true,
-    index: 0,
-  },
+  update() {
+    if (this.complete) {
+      this.complete = false;
+    } else {
+      this.complete = true;
+    }
+  }
+}
 
-  {
-    description: 'Brush my teeth',
-    completed: false,
-    index: 1,
-  },
+// Add items to UI
+const addToList = () => {
+  const todoList = document.getElementById('todo-list');
+  todoList.innerHTML = '';
 
-  {
-    description: 'Take my bath',
-    completed: true,
-    index: 3,
-  },
+  ToDo.list.forEach((item) => {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('id', item.index);
+    listItem.classList = 'item-container';
 
-  {
-    description: 'Go to school',
-    completed: false,
-    index: 4,
-  },
+    listItem.innerHTML = `
+    <input class="checkbox" type="checkbox">
+    <input type="text" class="input" value='${item.description}'>
+    <button class="cancel-btn"><i class="fa fa-trash" aria-hidden="true"></i></button>
+    `;
+    todoList.appendChild(listItem);
 
-  {
-    description: 'Go to movies',
-    completed: false,
-    index: 5,
-  },
+    const checkbox = listItem.querySelector('input');
+    const text = listItem.querySelector('.input');
+    const textInput = listItem.querySelector('p');
+    const deleteButton = listItem.querySelector('button');
 
-  {
-    description: 'Read a novel',
-    completed: false,
-    index: 6,
-  },
-];
+     // Update
+     checkbox.addEventListener('change', () => {
+      const index = parseInt(listItem.id, 10);
+      ToDo.list[index].update();
+      text.classList.toggle('complete');
+      textInput.classList.toggle('complete');
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+    });
 
-const show = (arr) => {
-  arr.forEach((e) => {
-    toDoContainer.innerHTML += `<div class="activity-container">
-    <input type="checkbox" class="checkBox">
-    <li class ="item">${arr[arr.indexOf(e)].description}</li>
-    <i class="fa-solid fa-ellipsis-vertical"></i>
-    </div>`;
+    text.addEventListener('input', (e) => {
+      text.value = e.target.value;
+      const index = parseInt(listItem.id, 10);
+      ToDo.list[index].description = e.target.value;
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+      if (e.code === 'Enter') {
+        // text.style.display = 'block';
+        textInput.classList.toggle('edit-item');
+      }
+    });
+
+    // Delete functionality
+    deleteButton.addEventListener('click', () => {
+      const index = parseInt(listItem.id, 10);
+      ToDo.list = ToDo.list.filter((item) => item !== ToDo.list[index]);
+      ToDo.list.forEach((item, i) => { item.index = i; });
+      localStorage.setItem('todoList', JSON.stringify(ToDo.list));
+      addToList();
+    });
+
+    if (item.complete) {
+      checkbox.checked = true;
+      text.classList = 'complete';
+    }
   });
 };
 
-show(Activities);
+function add(e) {
+  if (e.key === 'Enter') {
+    const newItem = new ToDo(this.value, false);
+    localStorage.setItem('todoList', JSON.stringify(newItem.getList()));
+    this.value = '';
+    addToList();
+  }
+}
+
+// //On Window load
+const list = JSON.parse(localStorage.getItem('todoList'));
+if (list) {
+  list.forEach((item) => new ToDo(item.description, item.complete));
+}
+
+// Add input event on todo
+const addInput = document.getElementById('add-input');
+addInput.addEventListener('keydown', add);
+
+addToList();
